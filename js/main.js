5d31c93256ebdbc81cb2244dd61fb07b638cca4d
Biju <75775006+Biju777@users.noreply.github.com>
2022-04-07 20:59:00 +0545

    Upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('#icon').click(function(){
+        $('ul').toggleClass('show');
+    });
+});
+
+$(document).ready(function(){
+    $('.hides').click(function(){
+        $('ul').toggleClass('hide');
+    });
+});
